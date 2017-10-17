537356d3efbe42bff5c41d1053a58700362ceb9c
markokeeffe <markokeeffe@vsware.ie>
2017-10-17 13:01:54 +0100

    State

--- App.jsx
+++ App.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+     super();
+
+     this.state = {
+        data:
+        [
+           {
+              "id":1,
+              "name":"Foo",
+              "age":"20"
+           },
+
+           {
+              "id":2,
+              "name":"Bar",
+              "age":"30"
+           },
+
+           {
+              "id":3,
+              "name":"Baz",
+              "age":"40"
+           }
+        ]
+     }
+  }
+
    render() {
      var i = 2;
 
@@ -10,9 +37,39 @@ class App extends React.Component {
      }
 
      return (
-        <Header/>
-        <Content/>
+        <div>
+           <Header/>
+           <table>
+              <tbody>
+                 {this.state.data.map((person, i) => <TableRow key = {i}
+                    data = {person} />)}
+              </tbody>
+           </table>
+        </div>
      );
+   }
+}
+
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.id}</td>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
+      );
    }
 }
 
